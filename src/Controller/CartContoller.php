5fd8f8804b458187ad80c3d58174c0da465214f9
marvint24 <70507842+marvint24@users.noreply.github.com>
2021-06-15 13:42:27 +0200

    cart

--- src/Controller/CartContoller.php
+++ src/Controller/CartContoller.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class CartContoller extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route('/test', name: 'test')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('cart/index.html.twig', [
+            'aktuelleZeiteinfach' => date("Y"),
         ]);
     }
 
