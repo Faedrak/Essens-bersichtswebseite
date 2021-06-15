ac9131d49e031256c98e6cf564c03923601d746f
Faedrak <l.juraschek@web.de>
2021-06-15 12:13:05 +0200

    Add Bootstrap

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Restaurant;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,12 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $restaurants = $this->getDoctrine()
+        ->getRepository(Restaurant::class)->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'restaurants' => $restaurants
         ]);
     }
 }
